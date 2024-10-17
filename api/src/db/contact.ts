1d5d0a84009de7290a07cf4a3499c4e3340027ee
chrisamirani <chrisamirani@yahoo.com>
2024-10-17 09:02:08 -0700

    unique emails

--- api/src/db/contact.ts
+++ api/src/db/contact.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const subscriptionsSchema = new mongoose.Schema(
   {
-    email: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
   },
   { timestamps: true }
 );
